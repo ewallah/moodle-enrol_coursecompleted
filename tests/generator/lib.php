0dca267a0906f0fc0b10b1236b490b144be5bbce
eWallah <info@eWallah.net>
2018-05-20 11:57:53 +0200

    generator

---
+++ tests/generator/lib.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * enrol_coursecompleted data generator.
+ *
+ * @package    enrol_coursecompleted
+ * @category   test
+ * @copyright  Renaat Debleu
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * enrol_coursecompleted data generator class.
+ *
+ * @package    enrol_coursecompleted
+ * @category   test
+ * @copyright  Renaat Debleu.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class enrol_coursecompleted_generator extends testing_module_generator {
+
+    public function create_instance($record = null, array $options = null) {
+        $record = (object)(array)$record;
+
+        // TODO for behats I think this is the reason for studentquiz 0!
+        if (!isset($record->name)) {
+            $record->name = 'test'
+        }
+
+        return parent::create_instance($record, (array)$options);
+    }
+}
