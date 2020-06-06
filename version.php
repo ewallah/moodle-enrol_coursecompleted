bdba90f24fed63f5223da76b090416f43e6cc39a
eWallah <info@eWallah.net>
2020-06-06 21:32:21 +0200

    version update

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020052400;
+$plugin->version = 2020060600;
 $plugin->requires  = 2019052000;
 $plugin->release = '3.7+';
 $plugin->maturity = MATURITY_STABLE;
