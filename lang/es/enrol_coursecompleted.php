6f2f3db5f00a28a21eb369ae6258496e9c5ce5f5
Fernando Peraza <fperaza@e-abclearning.com>
2023-04-21 13:39:18 -0400

    translated to spanish

---
+++ lang/es/enrol_coursecompleted.php
@@ -0,0 +1,77 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'enrol_coursecompleted', language 'en'.
+ *
+ * @package   enrol_coursecompleted
+ * @copyright 2017 eWallah (www.eWallah.net)
+ * @author    Renaat Debleu <info@eWallah.net>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['aftercourse'] = 'Después de completar el curso: {$a}';
+$string['cachedef_compcourses'] = 'Inscripción en la caché de finalización del curso';
+$string['coursecompleted:config'] = 'Configure enrol coursecompletion instances';
+$string['coursecompleted:manage'] = 'Administrar usuarios inscritos';
+$string['coursecompleted:enrolpast'] = 'Inscribir usuarios que completaron cursos en el pasado';
+$string['coursecompleted:unenrol'] = 'Desmatricular usuarios del curso';
+$string['coursecompleted:unenrolself'] = 'Desmatricualerse del curso';
+$string['compcourse'] = 'Curso completado';
+$string['compcourse_help'] = 'Cual curso deber ser completado.';
+$string['confirmbulkdeleteenrolment'] = 'Estás seguro de que quieres borrar estas inscripciones de usuarios?';
+$string['confirmbulkediteenrolment'] = 'Estás seguro de que quieres cambiar estas inscripciones?';
+$string['customwelcome'] = 'Mensaje personalizado de bienvenida';
+$string['customwelcome_help'] = 'Un mensaje personalizado de bienvenida puede ser agregado como texto plano o formato de Moodle, incluyendo etiquetas HTML y multi-lenguaje .
+
+
+
+The following placeholders may be included in the message:
+Los siguientes marcadores de posición pueden ser incluidos en el mensaje:
+
+* Nombre de curso {$a->coursename}
+* Nombre de curso completado {$a->completed}
+* Enlace a la página de perfil de usuario {$a->profileurl}
+* Correo de usuario {$a->email}
+* Nombre completo {$a->fullname}';
+$string['deleteselectedusers'] = 'Borrar matriculaciones seleccionadas en completación del curso';
+$string['editselectedusers'] = 'Editar matriculaciones seleccionadas en completación del curso';
+$string['editusers'] = 'Cambiar matriculaciones de ususario';
+$string['group'] = 'Mantener grupo';
+$string['group_help'] = 'Intentar agregar usuarios a un grupo con el mismo nombre';
+$string['keepgroup'] = 'Mantener configuracion de grupo por defecto';
+$string['keepgroup_help'] = 'Intentar agregar usuarios a un grupo con el mismo nombre por defecto';
+$string['pluginname'] = 'Matriculacion de curso completada';
+$string['pluginname_desc'] = 'El plugin de matrciullción de curso completado da acceso a otro curso cuando se complete un curso.';
+$string['privacy:metadata'] = 'l plugin de matrciullción de curso completado no guarda informacioón personal.';
+$string['processexpirationstask'] = 'Tarea expirada del plugin de matrciulación de curso completado';
+$string['status'] = 'Habilitado';
+$string['status_desc'] = 'Permite matriculaciones por curso completado por defecto.';
+$string['status_help'] = 'Esta configuración determina si la matriculación del curso completada está habilitada.';
+$string['status_link'] = 'Matriculación/cruso completado';
+$string['svglearnpath'] = 'Mostrar camino de aprendizaje';
+$string['svglearnpath_help'] = 'MuDisplay (possible) Camino de aprendizaje usando íconos svg.';
+$string['uponcompleting'] = 'Al terminar el curso {$a}';
+$string['usersenrolled'] = '{$a} Usuarios inscritos';
+$string['unenrolusers'] = 'Desmatricular usuarios';
+$string['welcome'] = 'Enviar mensaje de bienvenida al curso';
+$string['welcome_help'] = 'Cuando un usuario esta inscrito en un curso por terminar otro curso, se envía un mensaje de bienvenida por correo.';
+$string['welcometocourse'] = 'Bienvenido a {$a->coursename}!
+
+Felicitaciones!
+
+Luego de haber finalizado exitosamente {$a->completed}, tu estas automáticamente inscrito en el siguiente curso {$a->coursename}.';
+$string['willbeenrolled'] = 'Serás inscrito en este curso cuando termines este curso {$a}';
