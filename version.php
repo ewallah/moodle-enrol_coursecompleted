9f2b870ed8b36e30b707b2191e1056d03a692121
Renaat Debleu <rdebleu@eWallah.net>
2025-11-29 19:27:55 +0000

    Version update

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_coursecompleted';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v5.1.1';
+$plugin->release = 'v5.2.1';
 $plugin->version = 2025112900;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 501];
