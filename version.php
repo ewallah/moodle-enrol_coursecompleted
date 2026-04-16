90077103b3ee439c5f2156c26f7d1666592ec930
Renaat Debleu <rdebleu@eWallah.net>
2026-04-16 07:55:05 +0000

    MOODLE_502_STABLE

--- version.php
+++ version.php
@@ -30,4 +30,4 @@ $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v5.2.1';
 $plugin->version = 2025112900;
 $plugin->requires = 2025041400;
-$plugin->supported = [500, 501];
+$plugin->supported = [501, 502];
